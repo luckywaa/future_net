fd22d607d7c40ed8ca80ceace87e1826e9d2c29f
zhangsheng377 <435878393@qq.com>
2016-03-07 18:09:26 +0800

    First commit

---
+++ build/CMakeFiles/future_net.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zsd/桌面/huawei code craft 2016 初赛赛题包/project0/SDK-gcc/future_net/future_net.cpp" "/home/zsd/桌面/huawei code craft 2016 初赛赛题包/project0/SDK-gcc/build/CMakeFiles/future_net.dir/future_net.cpp.o"
+  "/home/zsd/桌面/huawei code craft 2016 初赛赛题包/project0/SDK-gcc/future_net/route.cpp" "/home/zsd/桌面/huawei code craft 2016 初赛赛题包/project0/SDK-gcc/build/CMakeFiles/future_net.dir/route.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/zsd/桌面/huawei code craft 2016 初赛赛题包/project0/SDK-gcc/future_net/lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
